64035e8b4c979121319f6eee1b82998fd198fa4f
Pedro Augusto Goncalves Gomes Amaral <paggamaral@sga.pucminas.br>
2026-08-31 11:58:29 -0300

    Removido import desnecessario de GeradorBin.java

--- TP01/GeradorBin.java
+++ TP01/GeradorBin.java
@@ -3,7 +3,6 @@ package TP01;
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
